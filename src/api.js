7a51288d3e7b79b3f0c5a8eb6430d27033336339
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-05-02 14:53:31 -0300

    feat: codigo da api catação de moeda

--- src/api.js
+++ src/api.js
@@ -1,10 +1,25 @@
+/**
+ * use: USD-BRL -> USDBRL -> dolar/real 
+ * use: EUR-BRL -> EURBRL -> euro/real
+ */
+
 async function getApiCotacao() {
-	const req = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL');
+	const req = await fetch('https://economia.awesomeapi.com.br/json/last/EUR-BRL');
 	const res = await req.json();
 
 	console.log(res)
+	const ver = res.EURBRL;
 
-	const msg = `$${res.USDBRL.ask}`;
-	document.getElementById('value').append(msg); 
+	const output = `
+		<h2 class='amount'>${ver.name}</h2>
+
+		<p class='amount-text'><b class='amount'>Alta $</b>${ver.high}</p>
+
+		<p class='amount-text'><b class='amount'>Baixa $</b>${ver.low}</p>
+
+		<span class='amount-text'><b class='amount'>DATA: </b>${ver.create_date}</span>
+
+	`
+	document.getElementById('value-da-api').innerHTML = output; 
 }
 getApiCotacao()
