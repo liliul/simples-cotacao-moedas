b94b966c3d31df33786e62b96dd1a14e2ae0e3b6
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-05-02 20:23:57 -0300

    feat: adicionando ask bid no html

--- src/api.js
+++ src/api.js
@@ -4,11 +4,11 @@
  */
 
 async function getApiCotacao() {
-	const req = await fetch('https://economia.awesomeapi.com.br/json/last/EUR-BRL');
+	const req = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL');
 	const res = await req.json();
 
 	console.log(res)
-	const ver = res.EURBRL;
+	const ver = res.USDBRL;
 
 	const output = `
 		<h2 class='amount'>${ver.name}</h2>
@@ -16,6 +16,10 @@ async function getApiCotacao() {
 		<p class='amount-text'><b class='amount'>Alta $</b>${ver.high}</p>
 
 		<p class='amount-text'><b class='amount'>Baixa $</b>${ver.low}</p>
+
+		<p class='amount-text'><b class='amount'>bid $</b>${ver.bid}</p>
+		
+		<p class='amount-text'><b class='amount'>ask $</b>${ver.ask}</p>
 
 		<span class='amount-text'><b class='amount'>DATA: </b>${ver.create_date}</span>
 
