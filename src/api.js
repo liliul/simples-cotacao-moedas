98c099e27feb4dcfdc4ec355f3a999a01651adda
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-05-04 22:09:33 -0300

    feat: function option adiciona tag html option no html

--- src/api.js
+++ src/api.js
@@ -4,26 +4,43 @@
  */
 
 async function getApiCotacao() {
-	const req = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL');
+	const req = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL');
 	const res = await req.json();
 
 	console.log(res)
-	const ver = res.USDBRL;
+	const element = res.USDBRL;
 
+	
 	const output = `
-		<h2 class='amount'>${ver.name}</h2>
+	<h2 class='amount'>${element.name}</h2>
 
-		<p class='amount-text'><b class='amount'>Alta $</b>${ver.high}</p>
+	<p class='amount-text'><b class='amount'>Alta $</b>${element.high}</p>
 
-		<p class='amount-text'><b class='amount'>Baixa $</b>${ver.low}</p>
+	<p class='amount-text'><b class='amount'>Baixa $</b>${element.low}</p>
 
-		<p class='amount-text'><b class='amount'>bid $</b>${ver.bid}</p>
-		
-		<p class='amount-text'><b class='amount'>ask $</b>${ver.ask}</p>
+	<p class='amount-text'><b class='amount'>bid $</b>${element.bid}</p>
+	
+	<p class='amount-text'><b class='amount'>ask $</b>${element.ask}</p>
 
-		<span class='amount-text'><b class='amount'>DATA: </b>${ver.create_date}</span>
+	<span class='amount-text'><b class='amount'>DATA: </b>${element.create_date}</span>
 
-	`
-	document.getElementById('value-da-api').innerHTML = output; 
+`
+	document.getElementById('value-da-api').innerHTML = output;
+	
+	 
 }
+function addOptions() {
+	const simbolos = ['USD','BRL','EUR','JPY'];
+
+	simbolos.map((simbolo) => {createOptions(simbolo)})
+}
+addOptions()
+function createOptions(textOption) {
+	const idSelect = document.getElementById('id-select');
+
+	const options = document.createElement('option');
+	options.text = textOption
+	idSelect.add(options, null);
+}
+
 getApiCotacao()
