82c7b278ec5f075df918ea14f9d5f38cc62b75ae
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-05-05 17:04:05 -0300

    feat: selecionar a cotação da moeda

--- src/api.js
+++ src/api.js
@@ -3,14 +3,38 @@
  * use: EUR-BRL -> EURBRL -> euro/real
  */
 
+
+window.addEventListener('change', getApiCotacao)
+getApiCotacao()
 async function getApiCotacao() {
-	const req = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL');
+	const req = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,USD-JPY,BTC-USD');
 	const res = await req.json();
 
 	console.log(res)
-	const element = res.USDBRL;
-
 	
+	const idSelect = document.getElementById('id-select');
+
+	const isOption = idSelect.options[idSelect.selectedIndex];
+	let isOptionText = Number(isOption.value); 
+	console.log(isOptionText)
+	const element = res.USDBRL;	
+	html(element)
+
+	if (isOptionText === 0) {
+		html(res.USDBRL)
+	}else if(isOptionText === 1) {
+		html(res.EURBRL)
+	}else if(isOptionText === 2) {
+		html(res.BTCBRL)
+	}else if(isOptionText === 3) {
+		html(res.USDJPY)
+	}else if(isOptionText === 4) {
+		html(res.BTCUSD)
+	}
+	
+}
+
+function html(element) {
 	const output = `
 	<h2 class='amount'>${element.name}</h2>
 
@@ -24,23 +48,23 @@ async function getApiCotacao() {
 
 	<span class='amount-text'><b class='amount'>DATA: </b>${element.create_date}</span>
 
-`
+	`
 	document.getElementById('value-da-api').innerHTML = output;
-	
-	 
 }
-function addOptions() {
-	const simbolos = ['USD','BRL','EUR','JPY'];
 
-	simbolos.map((simbolo) => {createOptions(simbolo)})
+function addOptions() {
+	const simbolos = ['USDBRL','EURBRL','BTCBRL','USDJPY','BTCUSD'];
+
+	simbolos.map((simbolo,index) => {createOptions(simbolo,index)})
 }
 addOptions()
-function createOptions(textOption) {
+
+let gerar = 0;
+function createOptions(textOption,valueOption) {
 	const idSelect = document.getElementById('id-select');
 
 	const options = document.createElement('option');
-	options.text = textOption
+	options.text  = textOption;
+	options.value = valueOption;
 	idSelect.add(options, null);
 }
-
-getApiCotacao()
