b8e2cd0dc6fd8827d28da5b7e834f1f5c4aa28e0
liliul <liliuestudo@gmail.com>
2023-07-13 22:51:12 -0300

    chore: removendo comentarios

--- src/hook/utils.js
+++ src/hook/utils.js
@@ -1,5 +1,3 @@
 export function converte(input1, input2) {
 	return input1 * input2
 }
-
-// input1.addEventListener('change', () => {document.getElementById('input-2').setAttribute('value', input1.value)})
